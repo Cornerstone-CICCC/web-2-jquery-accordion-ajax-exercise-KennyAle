c50fc675fbebd15085e1233a0ed9cb4625929c57
KennyAle <kennyviver@gmail.com>
2024-11-27 12:11:54 -0800

    Exercise

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 $(function() {
-  // your code here
+  $('.accordion-header').on('click', function() {
+    $(this).next('.accordion-content').slideToggle('fast',
+      function() {
+        $(this).siblings('.accordion-content').hide('fast')
+      }
+    )
+  })
+  $('.todos button').on('click', function() { 
+    $.ajax({
+      url: 'https://dummyjson.com/todos',
+      type: 'GET',
+      success: function(response) {
+        const data = response.todos
+        console.log(data)
+        data.forEach(todo => {
+          $('.todos ul').append(`<li>${todo.todo}</li>`)
+        });
+      },
+      error: function(error) {
+        console.eror(error)
+      }
+    })
+  })
 })
+
+
